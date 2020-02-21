1d1e9566bc6a8f9288182b76420a8f83d856dc0e
Marc Lieser <marc.lieser@hs-rm.de>
2020-02-21 13:22:27 +0100

    Thrust Range 2..20 N

--- model/quadrotor_model_thrustrates.cpp
+++ model/quadrotor_model_thrustrates.cpp
@@ -63,8 +63,8 @@ int main()
     const double g_z = 9.80665;      // Gravity is everywhere [m/s^2]
     const double w_max_yaw = 2;      // Maximal yaw rate [rad/s]
     const double w_max_xy = 3;       // Maximal pitch and roll rate [rad/s]
-    const double T_min = 00.01;      // Minimal thrust [N]
-    const double T_max = 17.63;      // Maximal thrust [N]
+    const double T_min = 2.00;       // Minimal thrust [N]
+    const double T_max = 20.00;      // Maximal thrust [N]
 
     // System Dynamics
     f << dot(p_x) == v_x;
